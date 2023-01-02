a0fa9306cd2dd936baf0dbe3b3bbc315fe289908
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-01-02 15:44:01 -0500

    Collectible Animation is relative to the viewport instead of the world, so it actually flies off screen

--- Assets/Scripts/Collectibles/Firefly.cs
+++ Assets/Scripts/Collectibles/Firefly.cs
@@ -11,16 +11,18 @@ using Helpers.Animation;
 namespace Collectibles {
     public class Firefly : Collectible, IFilterLoggerTarget
     {
-        [SerializeField] private float animSpeed;
-
+        private ViewportCurveAnimator _animator;
         public override string ID => "Firefly";
 
+        private void Awake()
+        {
+            _animator = GetComponent<ViewportCurveAnimator>();
+        }
         public override void OnTouched(Collector collector)
         {
             FilterLogger.Log(this, $"{gameObject.name} Touched {collector}");
-            StartCoroutine(CurveHelper.AnimateCurve(gameObject, GetAnimCurve(), animSpeed, () => OnFinishCollected(collector)));
+            _animator.PlayAnimation(() => OnFinishCollected(collector));
         }
-
 
         private void OnFinishCollected(Collector collector)
         {
@@ -28,31 +30,9 @@ namespace Collectibles {
             Destroy(gameObject);
         }
 
-        private Vector3 GetOffScreenPoint()
-        {
-            return Game.Instance.MainCamera.ViewportToWorldPoint(new Vector3(1, 1, Game.Instance.MainCamera.nearClipPlane));
-        }
-
-        private CubicCurve2D GetAnimCurve()
-        {
-            return new HermiteCurve2D(transform.position, GetOffScreenPoint(), -200 * Vector2.right, 50 * (Vector2.up + Vector2.right));
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            CubicCurve2D animCurve = GetAnimCurve();
-            List<Vector2> samples = animCurve.SamplePoints(50);
-
-            Gizmos.color = Color.green;
-            foreach (Vector2 sample in samples)
-            {;
-                Gizmos.DrawSphere(sample, 2);
-            }
-        }
-
         public LogLevel GetLogLevel()
         {
-            return LogLevel.Warning;
+            return LogLevel.Info;
         }
     }
 }
