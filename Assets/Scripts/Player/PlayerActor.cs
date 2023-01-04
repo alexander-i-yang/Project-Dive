91d22aaa3e6bf9a856160c93e370302127316e01
Alex Yang <alex@yang2k.com>
2023-01-03 19:12:48 -0600

    Removed neutral dogojump, reset spike prefab anchors

--- Assets/Scripts/Player/PlayerActor.cs
+++ Assets/Scripts/Player/PlayerActor.cs
@@ -110,21 +110,23 @@ public class PlayerActor : Actor, IFilterLoggerTarget {
         return v;
     }
 
-    public void DogoJump(int moveDirection, bool conserveMomentum, double oldXV)
-    {
-        if (moveDirection != 0)
+    public void DogoJump(int moveDirection, bool conserveMomentum, double oldXV) {
+        // if (moveDirection == 0) moveDirection = 
+        if (!(moveDirection == 1 || moveDirection == -1)) {
+            throw new ArgumentException(
+            $"Cannot dogo jump in direction({moveDirection})"
+            );
+        }
+        velocityX = moveDirection * PlayerCore.DogoJumpXV;
+        if (conserveMomentum)
         {
-            velocityX = moveDirection * PlayerCore.DogoJumpXV;
-            if (conserveMomentum)
+            if (moveDirection == 1)
             {
-                if (moveDirection == 1)
-                {
-                    velocityX = (float)Math.Max(oldXV + PlayerCore.DogoJumpXV, PlayerCore.DogoJumpXV);
-                }
-                else if (moveDirection == -1)
-                {
-                    velocityX = (float)Math.Min(oldXV - PlayerCore.DogoJumpXV, -PlayerCore.DogoJumpXV);
-                }
+                velocityX = (float)Math.Max(oldXV + PlayerCore.DogoJumpXV, PlayerCore.DogoJumpXV);
+            }
+            else if (moveDirection == -1)
+            {
+                velocityX = (float)Math.Min(oldXV - PlayerCore.DogoJumpXV, -PlayerCore.DogoJumpXV);
             }
         }
 
