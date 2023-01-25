268b39bd84e0470e1e8d09c779c91eb083da2b2a
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-01-24 21:41:43 -0500

    Rebuild FMOD banks and LightFlicker.cs update for animation curve

--- Assets/Scripts/Helpers/LightFlicker.cs
+++ Assets/Scripts/Helpers/LightFlicker.cs
@@ -1,14 +1,19 @@
 using Helpers;
+
+using System;
+
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class LightFlicker : MonoBehaviour
 {
-    [SerializeField, Range(0f, 1f)] private float percentOn = 0.5f;
+    [SerializeField, Range(0f, 1f)] private float flashChancePerUpdate = 0.5f;
     [SerializeField] private float updateFrequency = 1f;
+    [SerializeField] private AnimationCurve flashCurve;
     [SerializeField] private float minIntensity = 0.5f;
     [SerializeField] private float maxIntensity = 2f;
 
+    private float _targetIntensity;
     private Light2D _light;
     private GameTimer _updateTimer;
 
@@ -16,13 +21,18 @@ public class LightFlicker : MonoBehaviour
     {
         _light = GetComponent<Light2D>();
         _updateTimer = GameTimer.StartNewTimer(updateFrequency);
+
+        _targetIntensity = minIntensity;
     }
 
     private void Update()
     {
         if (GameTimer.TimerFinished(_updateTimer))
         {
-            _light.intensity = Random.value < percentOn ? maxIntensity : minIntensity;
+            if (UnityEngine.Random.value < flashChancePerUpdate)
+            {
+                _targetIntensity = _targetIntensity == minIntensity ? maxIntensity : minIntensity;
+            }
             GameTimer.Reset(_updateTimer);
         }
 
