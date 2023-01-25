d7562988cb6b5a30c7f138b9466f35af2e33a8e5
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-01-24 22:32:19 -0500

    Firefly Gradual Light

--- Assets/Scripts/Helpers/LightFlicker.cs
+++ Assets/Scripts/Helpers/LightFlicker.cs
@@ -1,3 +1,4 @@
+using Core;
 using Helpers;
 
 using System;
@@ -7,36 +8,45 @@ using UnityEngine.Rendering.Universal;
 
 public class LightFlicker : MonoBehaviour
 {
-    [SerializeField, Range(0f, 1f)] private float flashChancePerUpdate = 0.5f;
+    [SerializeField, Range(0f, 1f)] private float percentOn = 0.5f;
     [SerializeField] private float updateFrequency = 1f;
-    [SerializeField] private AnimationCurve flashCurve;
+    [SerializeField] private AnimationCurve intensityCurve;
+    [SerializeField] private float flashSpeed = 1f;
     [SerializeField] private float minIntensity = 0.5f;
     [SerializeField] private float maxIntensity = 2f;
 
-    private float _targetIntensity;
+    private bool _lightOn;
     private Light2D _light;
     private GameTimer _updateTimer;
+
+    private float _flashProgress;
+
+    private float TargetIntensity => _lightOn ? maxIntensity : minIntensity;
+    private float LastTargetIntensity => _lightOn ? minIntensity : maxIntensity;
 
     private void Awake()
     {
         _light = GetComponent<Light2D>();
         _updateTimer = GameTimer.StartNewTimer(updateFrequency);
 
-        _targetIntensity = minIntensity;
+        _lightOn = false;
     }
 
     private void Update()
     {
+        //Randomly change the intensity to on or off.
         if (GameTimer.TimerFinished(_updateTimer))
         {
-            if (UnityEngine.Random.value < flashChancePerUpdate)
-            {
-                _targetIntensity = _targetIntensity == minIntensity ? maxIntensity : minIntensity;
-            }
+            _lightOn = UnityEngine.Random.value < percentOn;
             GameTimer.Reset(_updateTimer);
         }
-
-
         GameTimer.Update(_updateTimer);
+
+        //Sample animation curve and update light intensity
+        _light.intensity = Mathf.MoveTowards(_light.intensity, TargetIntensity, flashSpeed * Time.deltaTime);
+        //float t = Mathf.InverseLerp(LastTargetIntensity, TargetIntensity, _light.intensity);
+        //t = Mathf.Clamp01(t + Game.Instance.DeltaTime * flashSpeed);
+        //float s = intensityCurve.Evaluate(t);
+        //_light.intensity = Mathf.Lerp(LastTargetIntensity, TargetIntensity, s);
     }
 }
