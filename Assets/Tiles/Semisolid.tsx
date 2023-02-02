bdec34f8e830b1ec531a5b08d4776e97d10e4764
Alex Yang <alex@yang2k.com>
2023-02-02 00:04:29 -0500

    Semisolids

---
+++ Assets/Tiles/Semisolid.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Semisolid" tilewidth="8" tileheight="8" tilecount="36" columns="6">
+ <image source="Semisolid.png" width="48" height="48"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="2"/>
+  </objectgroup>
+ </tile>
+</tileset>
