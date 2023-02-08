23ee9e4d8fda17a264535c23745d70e0970c1c35
Alex Yang <alex@yang2k.com>
2023-02-07 20:54:27 -0500

    Lighting fixes

--- Assets/Editor/TiledUtil/TileImporter.cs
+++ Assets/Editor/TiledUtil/TileImporter.cs
@@ -237,8 +237,8 @@ namespace TiledUtil {
 
         private void ImportBranchesTilemap(GameObject g)
         {
-            g.GetRequiredComponent<TilemapRenderer>().SetSortingLayer("Ground");
-            LayerImportLibrary.SetMaterial(g, "Mask_Graph");
+            g.GetRequiredComponent<TilemapRenderer>().SetSortingLayer("Above Ground");
+            // LayerImportLibrary.SetMaterial(g, "Mask_Graph");
         }
         
         private void ImportSpikesTilemap(GameObject g)
