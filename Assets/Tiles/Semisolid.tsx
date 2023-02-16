87bc1036758a773f1b0673870771f760dab3f5f3
Alex Yang <alex@yang2k.com>
2023-02-16 15:50:26 -0500

    Integrate Jonathan's Room

--- Assets/Tiles/Semisolid.tsx
+++ Assets/Tiles/Semisolid.tsx
@@ -1,64 +1,64 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Semisolid" tilewidth="8" tileheight="8" tilecount="36" columns="6">
- <image source="Semisolid.png" width="48" height="48"/>
- <tile id="9">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="10">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="16">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="17">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="18">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="19">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="22">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="23">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="25">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="26">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="32">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="33">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
+    <image source="Semisolid.png" width="48" height="48"/>
+    <tile id="9">
+    <objectgroup draworder="index" id="2">
+    <object id="2" x="0" y="0" width="8" height="8"/>
+    </objectgroup>
+</tile>
+<tile id="10">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="16">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="17">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="18">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="19">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="22">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="23">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="25">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="26">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="32">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
+<tile id="33">
+ <objectgroup draworder="index" id="2">
+  <object id="1" x="0" y="0" width="8" height="8"/>
+ </objectgroup>
+</tile>
 </tileset>
