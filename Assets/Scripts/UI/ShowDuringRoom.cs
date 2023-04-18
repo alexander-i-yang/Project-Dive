fcf51655c050c2a0d2de08ba1623d62e8570e113
Alex Yang <alex@yang2k.com>
2023-04-18 18:58:47 -0400

    Added Eldritch Horror

---
+++ Assets/Scripts/UI/ShowDuringRoom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using World;
+
+namespace UI
+{
+    public class ShowDuringRoom : MonoBehaviour
+    {
+        [SerializeField] private Room roomToShow;
+        [SerializeField] private FakeAnimator objToShow;
+        
+        private void OnEnable()
+        {
+            Room.RoomTransitionEvent += OnRoomTransition;
+        }
+        
+        private void OnDisable()
+        {
+            Room.RoomTransitionEvent -= OnRoomTransition;
+        }
+
+        private void OnRoomTransition(Room r)
+        {
+            if (r == roomToShow) objToShow.Show();
+            else objToShow.Hide();
+        }
+    }
+}
